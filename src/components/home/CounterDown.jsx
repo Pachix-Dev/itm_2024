4277d260a6252605db369546cfa29667b03de2e6
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-05-27 08:38:11 -0600

    feat: Add exhibitor profile components and update exhibitor profile page

--- src/components/home/CounterDown.jsx
+++ src/components/home/CounterDown.jsx
@@ -3,7 +3,7 @@ import { useCountdown } from '../../hooks/useCounterDown.js'
 export function CounterDown({ dias, horas, minutos, segundos }) {
   const { days, hours, minutes, seconds } = useCountdown('9 oct 2024 09:00:00')
   return (
-    <div className='text-white bg-transparent bg-opacity-50 backdrop-blur-md flex text-center md:grid md:text-left lg:flex lg:text-center justify-center gap-6 text-md md:text-3xl font-semibold py-5 px-5 md:px-8 lg:px-2 border rounded-lg'>
+    <div className='text-white bg-transparent bg-opacity-50 backdrop-blur-md flex text-center md:grid md:text-left lg:flex lg:text-center justify-center gap-6 text-md md:text-3xl font-semibold py-5 px-5 md:px-8 lg:px-2 xl:px-8 border rounded-lg'>
       <div>
         <span className='font-extrabold  text-4xl md:text-6xl'>{days}</span>{' '}
         {dias}
