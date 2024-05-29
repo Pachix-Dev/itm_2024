6bf1618290afc1c9ab2c2280283314973077f9a0
Pachi-Dev <pachi.claros@gmail.com>
2024-05-29 14:04:05 -0600

    feat: Add Industrial Transformation Mexico logos in different variations

--- src/components/shared/Gallery.jsx
+++ src/components/shared/Gallery.jsx
@@ -28,26 +28,71 @@ export default function SimpleGallery(props) {
 
   const Pagination = () => (
     <div className='flex justify-end p-5'>
-      <nav aria-label="Page navigation example">
-        <ul className="flex items-center -space-x-px h-8 text-sm">
+      <nav aria-label='Page navigation example'>
+        <ul className='flex items-center -space-x-px h-8 text-sm'>
           <li>
-            <button className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700" onClick={() => setPage(page === 0 ? 0 : page - 1)}>
-              <span className="sr-only">Previous</span>
-              <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+            <button
+              className='flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700'
+              onClick={() => setPage(page === 0 ? 0 : page - 1)}
+            >
+              <span className='sr-only'>Previous</span>
+              <svg
+                className='w-2.5 h-2.5 rtl:rotate-180'
+                aria-hidden='true'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 6 10'
+              >
+                <path
+                  fill='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M5 1 1 5l4 4'
+                />
               </svg>
             </button>
           </li>
           {imageGroups.map((_, index) => (
             <li key={index}>
-              <button className={`flex items-center justify-center px-3 h-8 leading-tight border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${page === index ? 'bg-gray-100 text-gray-700' : 'text-gray-500 bg-white'}`} onClick={() => setPage(index)}>{index + 1}</button>
+              <button
+                className={`flex items-center justify-center px-3 h-8 leading-tight border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${
+                  page === index
+                    ? 'bg-gray-100 text-gray-700'
+                    : 'text-gray-500 bg-white'
+                }`}
+                onClick={() => setPage(index)}
+              >
+                {index + 1}
+              </button>
             </li>
           ))}
           <li>
-            <button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700" onClick={() => setPage(page === imageGroups.length - 1 ? imageGroups.length - 1 : page + 1)}>
-              <span className="sr-only">Next</span>
-              <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+            <button
+              className='flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700'
+              onClick={() =>
+                setPage(
+                  page === imageGroups.length - 1
+                    ? imageGroups.length - 1
+                    : page + 1
+                )
+              }
+            >
+              <span className='sr-only'>Next</span>
+              <svg
+                className='w-2.5 h-2.5 rtl:rotate-180'
+                aria-hidden='true'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 6 10'
+              >
+                <path
+                  fill='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='m1 9 4-4-4-4'
+                />
               </svg>
             </button>
           </li>
@@ -61,7 +106,6 @@ export default function SimpleGallery(props) {
       {imageGroups.length > 1 && <Pagination />}
       <masonry-layout
         gap='24'
-        maxcolwidth='480'
         class='container mx-auto px-4'
         id={props.galleryID}
       >
--- src/data/constans_logos.js
+++ src/data/constans_logos.js
@@ -29,5 +29,237 @@ const exhibitors_2 = [
     { src: '/exhibitors/schneider_v2.webp', alt: '', width: 170 },
 ]
 
+const alliesStrategic = [
+    {
+      src: '/A3.webp',
+      width: 190,
+      height: 127,
+      alt: 'A3',
+      link: 'https://www.a3mexico.com.mx/a3',
+    },
+    {
+      src: '/ahk.webp',
+      width: 190,
+      height: 127,
+      alt: 'ahk',
+      link: 'https://mexiko.ahk.de/es/',
+    },
+    {
+      src: '/canieti.webp',
+      width: 190,
+      height: 127,
+      alt: 'canieti',
+      link: 'https://www.canieti.org/inicio.aspx',
+    },
+    {
+      src: '/giz.webp',
+      width: 190,
+      height: 127,
+      alt: 'giz',
+      link: 'https://www.giz.de/en/worldwide/33041.html',
+    },
+    {
+      src: '/gto200.webp',
+      width: 190,
+      height: 127,
+      alt: 'gto200',
+      link: 'https://www.gobiernoabiertogto.org.mx/',
+    },
+    {
+      src: '/dual.webp',
+      width: 190,
+      height: 127,
+      alt: 'dual',
+      link: 'https://aedual.mx/',
+    },
+    {
+      src: '/kas.webp',
+      width: 190,
+      height: 127,
+      alt: 'kas',
+      link: 'https://www.kas.de/es/web/mexiko',
+    },
+    {
+      src: '/pagsid.webp',
+      width: 190,
+      height: 127,
+      alt: 'pagsid',
+      link: 'https://www.kas.de/es/web/mexiko',
+    },
+    {
+      src: '/onudi.webp',
+      width: 190,
+      height: 127,
+      alt: 'onudi',
+      link: 'https://www.gob.mx/semarnat/acciones-y-programas/organizacion-de-las-naciones-unidas-para-el-desarrollo-industrial-onudi',
+    },
+    {
+      src: '/enpact.webp',
+      width: 190,
+      height: 127,
+      alt: 'enpact',
+      link: 'https://enpact.org/',
+    },
+    {
+      src: '/80amsoc.webp',
+      width: 190,
+      height: 127,
+      alt: '80 amsoc',
+      link: 'https://amsoc.mx/es/inicio/',
+    },
+    {
+      src: '/cinvestav.webp',
+      width: 190,
+      height: 127,
+      alt: 'cinvestav',
+      link: 'https://www.cinvestav.mx/',
+    },
+    {
+      src: '/camara-france.webp',
+      width: 200,
+      height: 106,
+      alt: 'cci-france',
+      link: 'https://www.franciamexico.com/',
+    },
+    {
+      src: '/cancham.webp',
+      width: 190,
+      height: 127,
+      alt: 'cancham',
+      link: 'https://www.canchammx.com/',
+    },
+    {
+      src: '/concamin.webp',
+      width: 200,
+      alt: 'concamin',
+      link: 'https://www.concamin.org.mx/inicio',
+    },
+]
 
-export { exhibitors_1, exhibitors_2 }
+const media_1 = [
+    {
+      src: '/EFE.webp',
+      width: 200,
+      height: 133,
+      alt: 'EFE',
+      link: 'https://efe.com/',
+    },
+    {
+      src: '/multimediosLogo.webp',
+      width: 200,
+      height: 155,
+      alt: 'Multimedios',
+      link: 'https://grupomultimedios.com/',
+    },
+    {
+      src: '/global-energy-v2.webp',   
+      width: 200,   
+      height: 70,
+      alt: 'GlobalEnergy',
+      link: 'https://globalenergy.mx/',
+    },
+    {
+      src: '/global-industries_v2.webp',
+      width: 200,  
+      height: 70,
+      alt: 'Global industries ',
+      link: 'https://globalindustries.mx/',
+    },
+    {
+      src: '/cluster.webp',
+      width: 200,
+      height: 133,
+      alt: 'cluster industrial',
+      link: 'https://www.clusterindustrial.com.mx/',
+    },
+    {
+      src: '/dpl.webp',
+      width: 200,
+      height: 76,
+      alt: 'dpl',
+      link: 'https://dplnews.com/',
+    },
+    {
+      src: '/Energy-Commerce.webp',
+      width: 200,
+      height: 70,
+      alt: 'energy commerce',
+      link: 'https://energyandcommerce.com.mx/',
+    },
+    {
+      src: '/mexicoindustry.webp',
+      width: 200,
+      height: 133,
+      alt: 'mexico industry',
+      link: 'https://mexicoindustry.com/',
+    },
+]
+const media_2 = [
+{
+    src: '/vanguardia.webp',
+    width: 200,
+    height: 133,
+    alt: 'vanguardia ',
+    link: 'https://www.vanguardia-industrial.net/',
+},
+{
+    src: '/am.webp',
+    width: 200,
+    height: 133,
+    alt: 'AM ',
+    link: 'https://www.am.com.mx/',
+},
+{
+    src: '/directorioauto.webp',
+    width: 200,
+    height: 133,
+    alt: 'Directorio automotriz ',
+    link: 'https://www.directorioautomotriz.com.mx/',
+},
+{
+    src: '/cuatrocero.webp',
+    width: 200,
+    height: 125,
+    alt: 'cuatro cero',
+    link: 'https://cuatro-cero.mx/',
+},
+{
+    src: '/revistaconsultoria.webp',
+    width: 200,
+    height: 133,
+    alt: 'revista consultora ',
+    link: 'https://revistaconsultoria.com.mx/',
+},
+{
+    src: '/partner12.webp',
+    width: 200,
+    alt: 'cosmos online ',
+    link: 'https://www.cosmos.com.mx/',
+},
+{
+    src: '/heraldoHorizontal.webp',
+    width: 200,
+    alt: 'Heraldo Mexico',
+    link: 'https://heraldodemexico.com.mx/',
+},
+{
+    src: '/manufacturalatam.webp',
+    width: 300,
+    alt: 'Manufactura LATAM',
+    link: 'https://www.manufactura-latam.com/es',
+},
+{
+    src: '/metalmecanica.webp',
+    width: 250,
+    alt: 'Metalmecánica',
+    link: 'https://www.metalmecanica.com/es',
+},
+{
+    src: '/Milenio.webp',
+    width: 200,
+    alt: 'Grupo Milenio',
+    link: 'https://www.milenio.com/',
+}
+] 
+
+export { exhibitors_1, exhibitors_2, media_1, media_2, alliesStrategic}
