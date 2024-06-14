df3af0ee92b8edc6215aea43c47b581c3defd526
Pachi-Dev <pachi.claros@gmail.com>
2024-06-13 18:12:56 -0600

    feat: Update footer images for VIP program

--- server/TemplateEmail.js
+++ server/TemplateEmail.js
@@ -2,65 +2,120 @@
 const email_template = async ({ name, paternSurname, maternSurname}) => {          
     
     return (
-   `<table align="center" width="100%" role="presentation" cellspacing="0" cellpadding="0" border="0" style="max-width: 680px; width: 100%; margin: 0px auto;">
-   <tbody>
-      <tr style="width: 100%;">
-       <td>
-         <img src="https://industrialtransformation.mx/headerITMmail.webp" alt="logo" style="width: 100%;">                             
-        </td>
-      </tr>
-      <tr>
-        <td align="right">
-          <img src="https://re-plus-mexico.igeco.mx/italianExhibitionGroup.webp" alt="logo" style="width: 80px;margin-top: 15px;">
-          <img src="https://re-plus-mexico.igeco.mx/deutscheMesse.webp" alt="logo" style="width: 150px;margin-left: 5px;">
-        </td>
-      </tr>
-      <tr>
-        <td width="100%">
-          <h2 style="margin:0;font-weight:bold;text-transform: uppercase;">
-            BIENVENIDO, ${name} ${paternSurname} ${maternSurname}. TU REGISTRO SE HA REALIZADO CON ÉXITO.
-          </h2>
-          <h2 style='text-align: center;'>GRACIAS POR FORMAR PARTE DE, <span style="color:#E94568;font-weight:bold;">ITM 2024</span>, CONSTRUYAMOS LA INDUSTRIA DEL MAÑANA</h2>                    
-        </td>      
-      </tr> 
-      <tr>
-        <td>
-          <div  style="text-align: justify;font-size:22px;">
-            Industrial Transformation Mexico es un evento interesante e inclusivo en el que los actores clave y los grupos objetivo se reúnen para crear redes y oportunidades de intercambio de conocimientos para el éxito empresarial.                     
-          </div>
-          <div style="text-align:center;padding:20px;margin-top:25px;">
-            <a style="background-color: #0E2242;color:white;padding:20px;border-radius:20px;margin:20px;text-decoration:none;" 
-            href="https://industrialtransformation.mx/programa" target='_blank'>                        
-              CONSULTA EL PROGRAMA DE ACTIVIDADES
-            </a>
-          </div>               
-          <p style="font-weight:bold;font-size:20px;text-align: center;">AÑADIR AL CALENDARIO</p>
-          <div style="text-align:center;padding:20px">
-            <a style="background-color: #0E2242;color:white;padding:20px;border-radius:20px;margin:20px;" href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20240608T180000Z%2F20240610T000000Z&details=Evento%20dise%C3%B1ado%20para%20el%20disfrute%20y%20el%20aprendizaje%20alrededor%20de%20esta%20bebida%20universal.%20Re%C3%BAne%20a%20expertos%2C%20amantes%20del%20vino%20y%20vi%C3%B1edos%20en%20un%20ambiente%20festivo%20y%20educativo.%20Es%20la%20plataforma%20perfecta%20para%20mostrar%20tus%20vinos%20y%20conectarte%20con%20nuevos%20consumidores%20y%20profesionales%20del%20sector.%0A&location=Distrito%20Le%C3%B3n%20MX%2C%20instalaciones%20de%20la%20feria&text=VIVE%20EL%20VINO"
-            target="_blank">                        
-              Google
-            </a>
-            <a style="background-color: #0E2242;color:white;padding:20px;border-radius:20px;" href="https://outlook.live.com/calendar/0/action/compose?allday=false&body=Evento%20dise%C3%B1ado%20para%20el%20disfrute%20y%20el%20aprendizaje%20alrededor%20de%20esta%20bebida%20universal.%20Re%C3%BAne%20a%20expertos%2C%20amantes%20del%20vino%20y%20vi%C3%B1edos%20en%20un%20ambiente%20festivo%20y%20educativo.%20Es%20la%20plataforma%20perfecta%20para%20mostrar%20tus%20vinos%20y%20conectarte%20con%20nuevos%20consumidores%20y%20profesionales%20del%20sector.%0A&enddt=2024-06-09T18%3A00%3A00&location=Distrito%20Le%C3%B3n%20MX%2C%20instalaciones%20de%20la%20feria&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2024-06-08T12%3A00%3A00&subject=VIVE%20EL%20VINO"
-            target="_blank">                       
-              Outlook
-            </a>
-          </div>                   
-          <div style="margin-top:10px;text-align:center;padding:20px;">
-            <a style="background-color: #0E2242;color:white;padding:20px;border-radius:20px;margin:20px;" href="https://calendar.yahoo.com/?desc=Evento%2520dise%25C3%25B1ado%2520para%2520el%2520disfrute%2520y%2520el%2520aprendizaje%2520alrededor%2520de%2520esta%2520bebida%2520universal.%2520Re%25C3%25BAne%2520a%2520expertos%252C%2520amantes%2520del%2520vino%2520y%2520vi%25C3%25B1edos%2520en%2520un%2520ambiente%2520festivo%2520y%2520educativo.%2520Es%2520la%2520plataforma%2520perfecta%2520para%2520mostrar%2520tus%2520vinos%2520y%2520conectarte%2520con%2520nuevos%2520consumidores%2520y%2520profesionales%2520del%2520sector.%250A&dur=false&et=20240610T000000Z&in_loc=Distrito+Le%C3%B3n+MX%2C+instalaciones+de+la+feria&st=20240608T180000Z&title=VIVE%2520EL%2520VINO&v=60"
-            target="_blank">
+   `<table width="100%" role="presentation" cellspacing="0" cellpadding="0" border="0"">
+    <tbody>
+        <tr style="width: 100%;">
+          <td colspan="2">
+            <img src="https://industrialtransformation.mx/headerITMmail.webp" alt="logo" style="width: 100%;">                             
+          </td>
+        </tr>
+        <tr>
+          <td align="right">
+            <img src="https://re-plus-mexico.igeco.mx/italianExhibitionGroup.webp" alt="logo" style="width: 80px;margin-top: 15px;">          
+            <img src="https://re-plus-mexico.igeco.mx/deutscheMesse.webp" alt="logo" style="width: 150px;margin-left: 5px;">
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" width="100%">
+            <h2 style="text-align: center;margin:0;font-weight:bold;text-transform: uppercase;margin-top: 20px;">
+              BIENVENIDO, ${name} ${paternSurname} ${maternSurname}. TU REGISTRO SE HA REALIZADO CON ÉXITO.
+            </h2>
+            <h2 style='text-align: center;'>
+              GRACIAS POR FORMAR PARTE DE <span style="color:#E94568;font-weight:bold;">ITM 2024</span>, CONSTRUYAMOS LA INDUSTRIA DEL MAÑANA
+            </h2>
+            <div style="text-align: justify;font-size:22px;">
+              Industrial Transformation Mexico es un evento interesante e inclusivo en el que los actores clave y los grupos objetivo se reúnen para crear redes y oportunidades de intercambio de conocimientos para el éxito empresarial.                     
+            </div>
+            <div style="text-align:center;padding:20px;margin-top:25px;">
+              <a style="background: rgb(228,9,102);background: linear-gradient(90deg, rgba(228,9,102,1) 0%, rgba(225,7,37,1) 100%);color:white;padding:20px;border-radius:20px;margin:20px;text-decoration:none;font-weight:bold;" 
+                href="https://industrialtransformation.mx/programa" target='_blank'>                        
+                CONSULTA EL PROGRAMA DE EVENTOS PARALELOS A ITM 2024
+              </a>
+              <p style="margin-top:40px;margin-bottom: 40px;">
+                Te esperamos en la sexta edición de INDUSTRIAL TRANSFORMATION MEXICO del <strong>9 al 11 de octubre, Poliforum, León.</strong>
+              </p>
+              <div style="background: rgb(228,9,102);background: linear-gradient(90deg, rgba(228,9,102,1) 0%, rgba(225,7,37,1) 100%);color:white;width:fit-content;border-radius: 20px;padding:20px;margin:auto;position:relative;">
+               
+               <p><strong>HORARIOS:</strong></p>
+               <p>Miércoles 9 y Jueves 10 de  11:00 am - 7:00 pm hrs</p>
+               <p>Viernes 11 de 11:00 am - 5:00 pm hrs</p>
+              </div>
+            </div>               
+            <p style="font-weight:bold;font-size:20px;text-align: center;">AÑADIR AL CALENDARIO</p>
+            <div style="text-align:center;padding:20px">
+              <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;margin:20px;text-decoration:none;" href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20241009T170000Z%2F20241012T010000Z&details=Industrial%20Transformation%20Mexico%20es%20un%20evento%20interesante%20e%20inclusivo%20en%20el%20que%20los%20actores%20clave%20y%20los%20grupos%20objetivo%20se%20re%C3%BAnen%20para%20crear%20redes%20y%20oportunidades%20de%20intercambio%20de%20conocimientos%20para%20el%20%C3%A9xito%20empresarial.%0A&location=Poliforum%2C%20Le%C3%B3n&text=INDUSTRIAL%20TRANSFORMATION%20MEXICO%202024"
+              target="_blank">                        
+                Google
+              </a>
+              <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;text-decoration:none;" href="https://outlook.live.com/calendar/0/action/compose?allday=false&body=Industrial%20Transformation%20Mexico%20es%20un%20evento%20interesante%20e%20inclusivo%20en%20el%20que%20los%20actores%20clave%20y%20los%20grupos%20objetivo%20se%20re%C3%BAnen%20para%20crear%20redes%20y%20oportunidades%20de%20intercambio%20de%20conocimientos%20para%20el%20%C3%A9xito%20empresarial.%0A&enddt=2024-10-11T19%3A00%3A00&location=Poliforum%2C%20Le%C3%B3n&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2024-10-09T11%3A00%3A00&subject=INDUSTRIAL%20TRANSFORMATION%20MEXICO%202024
+              target="_blank">                       
+                Outlook
+              </a>
+            </div>                   
+            <div style="margin-top:10px;text-align:center;padding:20px;">
+              <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;margin:20px;text-decoration:none;" href="https://calendar.yahoo.com/?desc=Industrial%2520Transformation%2520Mexico%2520es%2520un%2520evento%2520interesante%2520e%2520inclusivo%2520en%2520el%2520que%2520los%2520actores%2520clave%2520y%2520los%2520grupos%2520objetivo%2520se%2520re%25C3%25BAnen%2520para%2520crear%2520redes%2520y%2520oportunidades%2520de%2520intercambio%2520de%2520conocimientos%2520para%2520el%2520%25C3%25A9xito%2520empresarial.%250A&dur=false&et=20241012T010000Z&in_loc=Poliforum%2C+Le%C3%B3n&st=20241009T170000Z&title=INDUSTRIAL%2520TRANSFORMATION%2520MEXICO%25202024&v=60"
+              target="_blank">
+                
+              <i>Y!</i> Yahoo!
+              </a>
               
-            <i>Y!</i> Yahoo!
-            </a>
-            
-            <a style="background-color: #0E2242;color:white;padding:20px;border-radius:20px;" href="https://viveelvino.igeco.mx/descarga.ics"
-            donwload>                        
-              iCalendar
-            </a>
-          </div> 
-        </td>
-      </tr>   
-   </tbody>
-  </table>`
+              <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;text-decoration:none;" href="https://industrialtransformation.mx/icalendar.ics" donwload target="_blank">  
+                iCalendar
+              </a>
+            </div>
+            <p style="font-size:15px;line-height:21px;margin:16px 0px;font-weight:bold">
+              INSTRUCCIONES PARA TU VISITA:
+            </p>
+            <ul>
+              <li>
+                <strong>IMPORTANTE:</strong> Es indispensable llevar tu pre-registro impreso o en formato digital para agilizar tu acceso al evento.
+              </li>
+              <li>
+                Recuerda llevar tu credencial oficial de empresa o negocio para verificar tus datos.
+              </li>
+              <li>
+                Tu acceso es único e intransferible y debe estar visible durante toda tu visita.
+              </li>
+              <li>
+                En caso de perder tu gafete, no habrá reimpresiones en el piso expositor. Por lo cual, se tendrá que generar una compra de boleto para su ingreso con un costo de $300 MXN.
+              </li>                  
+            </ul>                                                          
+          </td>      
+        </tr>               
+        <tr>
+          <td style="width: 50%;padding-top:20px;" align="center">
+           <a style="background: rgb(228,9,102);background: linear-gradient(90deg, rgba(228,9,102,1) 0%, rgba(225,7,37,1) 100%);color:white;width:fit-content;border-radius: 20px;padding:20px;margin:auto;">
+            ENTÉRATE DE TODOS NUESTROS EVENTOS
+           </a>            
+          </td>
+          <td style="width: 50%;padding-top:20px;" align="center">
+            <a style="background: rgb(228,9,102);background: linear-gradient(90deg, rgba(228,9,102,1) 0%, rgba(225,7,37,1) 100%);color:white;width:fit-content;border-radius: 20px;padding:20px;margin:auto;text-decoration:none;" href="https://industrialtransformation.mx/files/Floorplan_ITM_2024_31.05.2023.pdf" target="_blank">
+              PLANO ITM 2024
+            </a>          
+          </td>
+        </tr> 
+        <tr>
+          <td style="width: 50%;padding-top:50px;" align="center">
+           <a style="background: rgb(228,9,102);background: linear-gradient(90deg, rgba(228,9,102,1) 0%, rgba(225,7,37,1) 100%);color:white;width:fit-content;border-radius: 20px;padding:20px;margin:auto;text-decoration:none;" href="https://industrialtransformation.mx/hotels" target="_blank">
+            RESERVA AQUÍ TU HOSPEDAJE CON TARIFA PREFERENCIAL
+           </a>            
+          </td>
+          <td style="width: 50%;padding-top:50px;" align="center">
+            <a style="background: rgb(228,9,102);background: linear-gradient(90deg, rgba(228,9,102,1) 0%, rgba(225,7,37,1) 100%);color:white;width:fit-content;border-radius: 20px;padding:20px;margin:auto;text-decoration:none;" href="https://industrialtransformation.mx" target="_blank">
+              INVITA A UN COLEGA
+            </a>          
+          </td>
+        </tr>       
+        <tr>
+          <td>
+          <hr style="width:100%;border-top:1px solid rgb(214,216,219);border-right:none rgb(214,216,219);border-bottom:none rgb(214,216,219);border-left:none rgb(214,216,219);margin:30px 0px">
+            <p style="font-size:12px;line-height:15px;margin:4px 0px;color:rgb(145,153,161);text-align:center">
+              <strong>IGECO</strong>, Blvrd Francisco Villa 102-piso 14, Oriental, 37510 León, Guanajuato México.
+            </p>
+          </td>   
+        </tr>     
+      </tbody>
+    </table>`
     )
   }
   
