e08da9dcf8870e19947405a33d1b6dd716435ea0
Pachi-Dev <pachi.claros@gmail.com>
2024-06-17 17:26:59 -0600

    Actualizar ancho de la tabla en TemplateEmail.js

--- server/TemplateEmail.js
+++ server/TemplateEmail.js
@@ -6,7 +6,7 @@ const email_template = async ({ name, paternSurname, maternSurname}) => {
     <tbody>
         <tr>
           <td colspan="2" align="center">
-            <img src="https://industrialtransformation.mx/header-ITM-register.jpg" alt="logo" width="500px";>                             
+            <img src="https://industrialtransformation.mx/header-ITM-register.jpg" alt="logo" width="500";>                             
           </td>
         </tr>        
         <tr>
