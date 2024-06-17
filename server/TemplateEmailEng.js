a2de249082e31add6bb67558c3233078bf854f9f
Pachi-Dev <pachi.claros@gmail.com>
2024-06-17 17:38:49 -0600

    Actualizar ancho de la tabla en TemplateEmail.js

--- server/TemplateEmailEng.js
+++ server/TemplateEmailEng.js
@@ -6,7 +6,7 @@ const email_template_eng = async ({ name, paternSurname, maternSurname}) => {
     <tbody>
         <tr style="width: 100%;">
           <td colspan="2">
-            <img src="https://industrialtransformation.mx/header-ITM-register-eng.jpg" alt="logo" style="width: 100%;">                             
+            <img src="https://industrialtransformation.mx/header-ITM-register-eng.jpg" alt="logo" style="max-width: 680px;">                             
           </td>
         </tr>
         <tr>
@@ -16,7 +16,7 @@ const email_template_eng = async ({ name, paternSurname, maternSurname}) => {
           </td>
         </tr>
         <tr>
-          <td colspan="2" width="100%">
+          <td colspan="2">
             <h2 style="text-align: center;margin:0;font-weight:bold;text-transform: uppercase;margin-top: 20px;">
               WELCOME, ${name} ${paternSurname} ${maternSurname}. YOUR REGISTRATION WAS SUCCESSFUL.
             </h2>
@@ -43,24 +43,27 @@ const email_template_eng = async ({ name, paternSurname, maternSurname}) => {
               </div>
             </div>               
             <p style="font-weight:bold;font-size:20px;text-align: center;">ADD TO CALENDAR</p>
-            <div style="text-align:center;padding:20px">
+            <div style="text-align:center;padding:20px;margin: 20px;">
               <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;margin:20px;text-decoration:none;" href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20241009T170000Z%2F20241012T010000Z&details=Industrial%20Transformation%20Mexico%20es%20un%20evento%20interesante%20e%20inclusivo%20en%20el%20que%20los%20actores%20clave%20y%20los%20grupos%20objetivo%20se%20re%C3%BAnen%20para%20crear%20redes%20y%20oportunidades%20de%20intercambio%20de%20conocimientos%20para%20el%20%C3%A9xito%20empresarial.%0A&location=Poliforum%2C%20Le%C3%B3n&text=INDUSTRIAL%20TRANSFORMATION%20MEXICO%202024"
               target="_blank">                        
                 Google
-              </a>
-              <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;text-decoration:none;" href="https://outlook.live.com/calendar/0/action/compose?allday=false&body=Industrial%20Transformation%20Mexico%20es%20un%20evento%20interesante%20e%20inclusivo%20en%20el%20que%20los%20actores%20clave%20y%20los%20grupos%20objetivo%20se%20re%C3%BAnen%20para%20crear%20redes%20y%20oportunidades%20de%20intercambio%20de%20conocimientos%20para%20el%20%C3%A9xito%20empresarial.%0A&enddt=2024-10-11T19%3A00%3A00&location=Poliforum%2C%20Le%C3%B3n&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2024-10-09T11%3A00%3A00&subject=INDUSTRIAL%20TRANSFORMATION%20MEXICO%202024
+              </a>                           
+            </div>
+            <div style="text-align:center;padding:20px;margin: 20px;">
+                 <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;text-decoration:none;" href="https://outlook.live.com/calendar/0/action/compose?allday=false&body=Industrial%20Transformation%20Mexico%20es%20un%20evento%20interesante%20e%20inclusivo%20en%20el%20que%20los%20actores%20clave%20y%20los%20grupos%20objetivo%20se%20re%C3%BAnen%20para%20crear%20redes%20y%20oportunidades%20de%20intercambio%20de%20conocimientos%20para%20el%20%C3%A9xito%20empresarial.%0A&enddt=2024-10-11T19%3A00%3A00&location=Poliforum%2C%20Le%C3%B3n&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2024-10-09T11%3A00%3A00&subject=INDUSTRIAL%20TRANSFORMATION%20MEXICO%202024
               target="_blank">                       
                 Outlook
               </a>
             </div>                   
-            <div style="margin-top:10px;text-align:center;padding:20px;">
+            <div style="text-align:center;padding:20px;margin: 20px;">
               <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;margin:20px;text-decoration:none;" href="https://calendar.yahoo.com/?desc=Industrial%2520Transformation%2520Mexico%2520es%2520un%2520evento%2520interesante%2520e%2520inclusivo%2520en%2520el%2520que%2520los%2520actores%2520clave%2520y%2520los%2520grupos%2520objetivo%2520se%2520re%25C3%25BAnen%2520para%2520crear%2520redes%2520y%2520oportunidades%2520de%2520intercambio%2520de%2520conocimientos%2520para%2520el%2520%25C3%25A9xito%2520empresarial.%250A&dur=false&et=20241012T010000Z&in_loc=Poliforum%2C+Le%C3%B3n&st=20241009T170000Z&title=INDUSTRIAL%2520TRANSFORMATION%2520MEXICO%25202024&v=60"
               target="_blank">
                 
               <i>Y!</i> Yahoo!
-              </a>
-              
-              <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;text-decoration:none;" href="https://industrialtransformation.mx/icalendar.ics" donwload target="_blank">  
+              </a>                            
+            </div>
+            <div style="text-align:center;padding:20px;margin: 20px;">
+                <a style="background: rgb(18,172,255);background: linear-gradient(90deg, rgba(18,172,255,1) 0%, rgba(10,61,255,1) 100%);color:white;padding:20px;border-radius:20px;text-decoration:none;" href="https://industrialtransformation.mx/icalendar.ics" donwload target="_blank">  
                 iCalendar
               </a>
             </div>
@@ -84,14 +87,14 @@ const email_template_eng = async ({ name, paternSurname, maternSurname}) => {
           </td>      
         </tr>               
         <tr>
-          <td style="width: 50%;padding-top:20px;" align="center">
+          <td style="padding:20px;" align="center">
             <p style="background: #E42128;width:fit-content;border-radius: 20px;padding:20px;margin:auto;">
               <a style="text-decoration:none;color:white;">
                 FIND OUT ABOUT ALL OUR EVENTS
               </a>
             </p>          
           </td>
-          <td style="padding-top:20px;" align="center">
+          <td style="padding:20px;" align="center">
             <p style="background: #E42128;width:fit-content;border-radius: 20px;padding:20px;margin:auto;">
               <a style="text-decoration:none;color:white;" href="https://industrialtransformation.mx/files/Floorplan_ITM_2024_31.05.2023.pdf" target="_blank">
                 ITM 2024 FLOOR PLAN 
@@ -100,14 +103,14 @@ const email_template_eng = async ({ name, paternSurname, maternSurname}) => {
           </td>
         </tr> 
         <tr>
-          <td style="padding-top:50px;" align="center">
+          <td style="padding:20px;" align="center">
            <p style="background: #E42128;width:fit-content;border-radius: 20px;padding:20px;margin:auto;">
             <a style="text-decoration:none;color:white;" href="https://industrialtransformation.mx/hotels" target="_blank">
               BOOK HERE YOUR LODGING<br /> WITH PREFERENTIAL RATES
             </a>
            </p>           
           </td>
-          <td style="padding-top:50px;" align="center">
+          <td style="padding:20px;" align="center">
             <p style="background: #E42128;width:fit-content;border-radius: 20px;padding:20px;margin:auto;">
               <a style="text-decoration:none;color:white;" href="https://industrialtransformation.mx/registro" target="_blank">
                 INVITE A COLLEAGUE
