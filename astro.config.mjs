68b87886d6ac9990fa107188489e55f661eddb8b
Pachi-Dev <pachi.claros@gmail.com>
2024-06-18 09:51:40 -0600

    Actualizar configuración de Astro para agregar integración de sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroI18next()]
+  site: "https://industrialtransformation.mx",
+  integrations: [tailwind(), react(), astroI18next(), sitemap({
+    i18n: {
+      defaultLocale: "es",
+      locales: {
+        en: "en",
+        es: "es"
+      }
+    }
+  })]
 });
