6ecd284ea97ecda71704ae421bc285c3fc799823
Pachi-Dev <pachi.claros@gmail.com>
2024-08-06 15:17:32 -0600

    feat: Update client ID in CheckoutForm.jsx for Oktoberfest checkout

--- src/components/oktoberfest/CheckoutForm.jsx
+++ src/components/oktoberfest/CheckoutForm.jsx
@@ -42,8 +42,8 @@ export function CheckoutForm({ currentLanguage, translate }) {
 
   const initialOptions = {
     clientId:
-      //'AZFPcXoEcNQyoYLYV-jTqEi3_ufNa3cPvWkEg741EYryPSimjaBL45KMgWHjVWd90Qn_QHT1lFI0sZUn',
-      'ASXsJEVjLguO7vsn4IOGxaFeZp0FzEkMErWxv6Foin2-niMwBWX_0ryUceIDzdiD-WNQy-WvdMx-lIwf',
+      'AZFPcXoEcNQyoYLYV-jTqEi3_ufNa3cPvWkEg741EYryPSimjaBL45KMgWHjVWd90Qn_QHT1lFI0sZUn',
+    //'ASXsJEVjLguO7vsn4IOGxaFeZp0FzEkMErWxv6Foin2-niMwBWX_0ryUceIDzdiD-WNQy-WvdMx-lIwf',
     currency: 'MXN',
     intent: 'capture',
   }
