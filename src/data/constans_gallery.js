2ce5b0a7f4f2483467ed13a51421fe2ea43572a8
Pachi-Dev <pachi.claros@gmail.com>
2024-10-11 08:59:45 -0600

    cambios

--- src/data/constans_gallery.js
+++ src/data/constans_gallery.js
@@ -1,16 +1,16 @@
 const home = [
-    { src: '/img/gallery/home/galeriaITM4.webp', width: 1024, height: 683 },
-    { src: '/img/gallery/home/galeriaITM8.webp', width: 1024, height: 683 },
-    { src: '/img/gallery/home/galeriaITM9.webp', width: 1024, height: 576 },
-    { src: '/img/gallery/home/galeriaITM1.webp', width: 1024, height: 576 },
-    { src: '/img/gallery/home/galeriaITM2.webp', width: 1024, height: 576 },
-    { src: '/img/gallery/home/galeriaITM3.webp', width: 1024, height: 576 },
-    { src: '/img/gallery/home/galeriaITM5.webp', width: 1024, height: 683 },
-    { src: '/img/gallery/home/galeriaITM6.webp', width: 1024, height: 683 },
-    { src: '/img/gallery/home/galeriaITM10.webp', width: 1024, height: 683 },
-    { src: '/img/gallery/2023/conferencias1.jpg', width: 1024, height: 683 },
-    { src: '/img/gallery/2023/startuppitch1.jpg', width: 1024, height: 683 },
-    { src: '/img/gallery/2023/octoker1.jpg', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_1.webp', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_2.webp', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_3.webp', width: 1024, height: 576 },
+    { src: '/img/gallery/2024/itm_2024_4.webp', width: 1024, height: 576 },
+    { src: '/img/gallery/2024/itm_2024_5.webp', width: 1024, height: 576 },
+    { src: '/img/gallery/2024/itm_2024_6.webp', width: 1024, height: 576 },
+    { src: '/img/gallery/2024/itm_2024_7.webp', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_8.webp', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_9.webp', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_10.webp', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_11.webp', width: 1024, height: 683 },
+    { src: '/img/gallery/2024/itm_2024_12.webp', width: 1024, height: 683 },
   ]
 
 export {home}
