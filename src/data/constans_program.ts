a82c1428c87483211852d5702b1dcf27ef1a128e
Pachi-Dev <pachi.claros@gmail.com>
2025-10-20 16:48:32 -0600

    feat: Agregar nuevos ponentes Álvaro Ruíz, Gustavo Emiliano Fauez, Pamela González y José Pablo Morales al programa, incluyendo sus biografías y fotos

--- src/data/constans_program.ts
+++ src/data/constans_program.ts
@@ -25,6 +25,29 @@ interface Program {
 //Transformation Area
 const stage_1_day_1: Program[] = [
     {
+        title: "Del terreno al gemelo digital: un solo ecosistema Pix4D ",
+        title_en: "From the Field to the Digital Twin: A Single Pix4D Ecosystem",
+        description: "En un mundo donde la precisión y la eficiencia son clave, Pix4D ofrece un ecosistema completo que conecta cada etapa del flujo de trabajo: desde la captura de datos con drones o cámaras, hasta la creación de gemelos digitales precisos listos para análisis, colaboración y toma de decisiones. Con una suite de soluciones integradas, Pix4D permite transformar imágenes en información geoespacial confiable, optimizando procesos en topografía, construcción, agricultura, minería y más. Todo dentro de un solo ecosistema que garantiza continuidad, calidad y resultados medibles.",
+        description_en: "In a world where precision and efficiency are paramount, Pix4D offers a comprehensive ecosystem that connects every stage of the workflow: from data capture with drones or cameras to the creation of accurate digital twins ready for analysis, collaboration, and decision-making. With an integrated suite of solutions, Pix4D enables the transformation of images into reliable geospatial information, optimizing processes in surveying, construction, agriculture, mining, and more. All within a single ecosystem that ensures continuity, quality, and measurable results.",
+        time: "12:15 hrs",
+        duration: "45 min",
+        speakers: [
+            {
+                id: "1",
+                name: "Álvaro Ruíz",
+                position: "Business Development Central LATAM",
+                position_en: "Business Development Central LATAM",
+                company: "PIX4D",
+                bio: "Álvaro Ruiz es responsable del área de Desarrollo de Negocios para Latinoamérica Central en Pix4D, empresa líder mundial en soluciones de fotogrametría y modelado 3D profesional. Desde su rol, promueve la implementación del ecosistema Pix4D en sectores estratégicos como la minería, agricultura, energía y construcción, ayudando a las organizaciones a optimizar sus operaciones mediante drones, LiDAR, BIM y GIS. Con una visión orientada a la innovación y la sostenibilidad, Álvaro impulsa proyectos que conectan la tecnología de Pix4D con las necesidades reales de la industria, fomentando la adopción de herramientas inteligentes para una toma de decisiones más precisa y basada en datos. Su experiencia en alianzas estratégicas y desarrollo regional lo posiciona como un referente en la transformación digital del sector geoespacial en América Latina.",
+                bio_en: "Álvaro Ruiz is responsible for the Business Development area for Central Latin America at Pix4D, a world leader in photogrammetry and professional 3D modeling solutions. From his role, he promotes the implementation of the Pix4D ecosystem in strategic sectors such as mining, agriculture, energy, and construction, helping organizations optimize their operations through drones, LiDAR, BIM, and GIS. With a vision oriented towards innovation and sustainability, Álvaro drives projects that connect Pix4D technology with the real needs of the industry, fostering the adoption of intelligent tools for more precise and data-driven decision-making. His experience in strategic alliances and regional development positions him as a benchmark in the digital transformation of the geospatial sector in Latin America.",
+                image: "/img/speakers/AlvaroRuiz.webp",
+                email: "",
+                phone: "",
+                moderador: true
+            },
+        ]
+    },
+    {
 
         "title": "El viaje de la energía hacia la era digital",
         "title_en": "The energy journey to the digital age",
@@ -120,6 +143,29 @@ const stage_1_day_1: Program[] = [
                 "email": "",
                 "phone": "",
                 "moderador": false
+            },
+        ]
+    },
+    {
+        title: "Los Drones no reemplazan pero equilibran capacidades ",
+        title_en: "Drones Do Not Replace but Balance Capabilities",
+        description: "La Conferencia trata de como los drones han ganado un protagonismo enorme en la actualidad equilibrando desventajas, que antes eran muy muy costosas de equilibrar, ejemplo drones de rescate en misiones SAR, drones de combate, Drones contra incendios, drones de seguridad, drones de entrega de mensajería, etc. Y en este devenir, como cada día se siguen perfeccionando, y desbloqueando nuevas aptitudes",
+        description_en: "The Conference discusses how drones have gained enormous prominence today by balancing disadvantages that were previously very costly to address, such as rescue drones in SAR missions, combat drones, firefighting drones, security drones, delivery drones, etc. And in this evolution, how they continue to be perfected every day, unlocking new capabilities.",
+        time: "15:20 hrs",
+        duration: "40 min",
+        speakers: [
+            {
+                id: "1",
+                name: "Gustavo Emiliano Fauez",
+                position: "CEO",
+                position_en: "CEO",
+                company: "Roca Defense & Systems",
+                bio: "Gustavo Emiliano Fauez, empresario Argentino, con empresas en México, Argentina,  Australia, Irlanda y USA, Ex Militar, Infante de Marina Argentino, Veterano Casco Azul en la MINUSTAH (Misión de las Naciones Unidas para la Estabilización en Haití) 2006-7, CEO y  fundador de Roca Defense & Systems, que es un Holding de empresas en Argentina, México y USA, dedicado a la investigación y desarrollo de software y hardware para la integración de  vehículos no tripulados, programas de capacitación y productos complementarios, inherentes a necesidades de Defensa, agroindustria, protección civil, seguridad industrial, búsqueda y rescate, etc",
+                bio_en: "Gustavo Emiliano Fauez, Argentine businessman, with companies in Mexico, Argentina, Australia, Ireland, and the USA. Former Military, Argentine Marine Infantryman, Blue Helmet Veteran in MINUSTAH (United Nations Stabilization Mission in Haiti) 2006-7. CEO and founder of Roca Defense & Systems, which is a holding company with businesses in Argentina, Mexico, and the USA, dedicated to research and development of software and hardware for the integration of unmanned vehicles, training programs, and complementary products related to the needs of Defense, agro-industry, civil protection, industrial security, search and rescue, etc.",
+                image: "/img/speakers/Gustavo_Emiliano_Fauez.webp",
+                email: "",
+                phone: "",
+                moderador: true
             },
         ]
     },
@@ -672,23 +718,41 @@ const stage_1_day_3: Program[] = [
         ]
     },
     {
-        "title": "",
-        "title_en": "",
-        "description": "",
-        "description_en": "",
-        "time": "15:50 hrs",
-        "duration": "35 min",
-        "speakers": []
-    },
-    {
-        "title": "",
-        "title_en": "",
-        "description": "",
-        "description_en": "",
-        "time": "16:30 hrs",
-        "duration": "30 min",
-        "speakers": []
-    },
+        title: "GeoField: Tecnología que revela lo invisible y transforma la agricultura",
+        title_en: "GeoField: Technology that Reveals the Invisible and Transforms Agriculture ",
+        description: "GeoField: Tecnología que revela lo invisible en la agricultura moderna” es una conferencia que muestra cómo los drones multiespectrales y el análisis de datos están transformando la forma de entender el campo. A través de imágenes que capturan lo que el ojo humano no puede ver, GeoField permite detectar estrés hídrico, deficiencias nutricionales y afectaciones tempranas antes de que impacten la producción. La charla expone casos reales, beneficios económicos y el futuro de la agricultura basada en datos. Es una invitación a los productores y técnicos a adoptar decisiones precisas, sustentadas en evidencia científica y tecnología avanzada.",
+        description_en: "GeoField: Technology that Reveals the Invisible in Modern Agriculture” is a conference that shows how multispectral drones and data analysis are transforming the way we understand the field. Through images that capture what the human eye cannot see, GeoField allows for the detection of water stress, nutritional deficiencies, and early impacts before they affect production. The talk presents real cases, economic benefits, and the future of data-driven agriculture. It is an invitation for producers and technicians to make precise decisions based on scientific evidence and advanced technology.",
+        time: "16:10 hrs",
+        duration: "35 min",
+        speakers: [
+          {
+                id: "1",
+                name: "Pamela González Camacho",
+                position: "Directora de soluciones geoespaciales",
+                position_en: "Geospatial Solutions Director",
+                company: "Grupo geospectral",
+                bio: "Ingeniera Geofísica egresada de la Facultad de Ingeniería de la UNAM, con especialización en Ciencias Atmosféricas y Sistemas de Información Geográfica. Actualmente es Directora de Soluciones Geoespaciales en Grupo GeoSpectral, donde lidera proyectos fotogramétricos, topográficos y multiespectrales con drones DJI, AUTEL y WINGTRA, además de impartir capacitación técnica en análisis geoespacial. Cuenta con experiencia en teledetección, análisis espacial, modelado digital del terreno y gestión de datos geográficos. Piloto certificada de RPAS, ha colaborado con instituciones públicas y privadas en la implementación de sistemas geográficos y monitoreo ambiental, combinando precisión técnica y visión estratégica para la transformación digital del sector agrícola.",
+                bio_en: "A Geophysical Engineer graduated from the Faculty of Engineering at UNAM, with a specialization in Atmospheric Sciences and Geographic Information Systems. She is currently the Geospatial Solutions Director at Grupo GeoSpectral, where she leads photogrammetric, topographic, and multispectral projects using DJI, AUTEL, and WINGTRA drones, as well as providing technical training in geospatial analysis. She has experience in remote sensing, spatial analysis, digital terrain modeling, and geographic data management. A certified RPAS pilot, she has collaborated with public and private institutions in implementing geographic systems and environmental monitoring, combining technical precision and strategic vision for the digital transformation of the agricultural sector.",
+                image: "/img/speakers/pamelagonzalez.webp",
+                email: "",
+                phone: "",
+                moderador: true
+            },
+            {
+                id: "2",
+                name: "José Pablo Morales Alfonso",
+                position: "Ejecutivo",
+                position_en: "Executive",
+                company: "Geofield",
+                bio: "Profesional en análisis de datos, logística y gestión empresarial, con experiencia en optimización de procesos y desarrollo estratégico de nuevos mercados. Actualmente forma parte de GeoSpectral, donde impulsa la expansión comercial y la integración de soluciones tecnológicas aplicadas al sector geoespacial. Ha desempeñado roles clave en empresas como PK Flex, Byggir & Co. y Essity, especializándose en control de operaciones, pronósticos de demanda y generación de indicadores mediante SQL, Python y Power BI. Su enfoque combina inteligencia de datos y visión empresarial para fortalecer la eficiencia operativa y la toma de decisiones en entornos de innovación tecnológica.",
+                bio_en: "A professional in data analysis, logistics, and business management, with experience in process optimization and strategic development of new markets. She is currently part of GeoSpectral, where she drives commercial expansion and the integration of technological solutions applied to the geospatial sector. She has held key roles in companies such as PK Flex, Byggir & Co., and Essity, specializing in operations control, demand forecasting, and generating indicators using SQL, Python, and Power BI. Her approach combines data intelligence and business vision to strengthen operational efficiency and decision-making in technological innovation environments.",
+                image: "/img/speakers/josepablomorales.webp",
+                email: "",
+                phone: "",
+                moderador: true
+            }
+        ],
+      },
 ]
 
 //Leaders of Tomorrow Area
